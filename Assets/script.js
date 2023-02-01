abfef4fcf6e94968cf1e607bff0cd3203f9c106d
Wes Harrison <wesharrison89@gmail.com>
2023-02-01 00:45:07 -0800

    added more bootstrap and js

---
+++ Assets/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+function getApi() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+
+    fetch(requestUrl)
+    .then(function (response){
+        return response.json();
+    })
+}
