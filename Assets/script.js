2059df53c837edf49fb67c91545aaa59c7f10c2c
Wes Harrison <wesharrison89@gmail.com>
2023-02-02 14:48:18 -0800

    added html structure

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,7 @@
 
 
 function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={33ba9af51fbe9a62543d67bc2a30b9e8}';
 
     fetch(requestUrl)
     .then(function (response){
