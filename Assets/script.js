849b5d5f7fe7c2ac8a2107bda96153b17f584525
Wes Harrison <wesharrison89@gmail.com>
2023-02-05 00:18:44 -0800

    updated new js file with variables and functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,22 @@
 var APIkey = '33ba9af51fbe9a62543d67bc2a30b9e8'
-var city = 'seattle';
+var city = 'miami';
 var fiveday = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid='+APIkey;
 var submitBtn = document.getElementById('btn');
 
+
+var cityInputEl = document.getElementById('city-input');
+var previousCityEl = document.getElementById('history');
+
+
+
+
 function getWeather(){
 
-   // var searchcity = document.getElementById('city-input');
+ var city=cityInputEl.value.trim();
+   if(city) {
+      cityInfoEl(city);
+      previousCityEl.textContent = '';
+   }
    
 
     fetch(fiveday)
