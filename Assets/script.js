4333b388c8be5a8c75aed7bac8aba846ab27911b
Wes Harrison <wesharrison89@gmail.com>
2023-02-06 14:27:06 -0800

    adding functions to js file

--- Assets/script.js
+++ Assets/script.js
@@ -1,34 +1,43 @@
-var APIkey = '33ba9af51fbe9a62543d67bc2a30b9e8'
-var city = 'miami';
-var fiveday = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid='+APIkey;
-var submitBtn = document.getElementById('btn');
 
-
+var APIkey = '7a7c04d503977a6da6ac8ba6bdb48f18';
 var cityInputEl = document.getElementById('city-input');
+var submitBtn = document.querySelector('.btn');
 var previousCityEl = document.getElementById('history');
+var fiveResults = document.querySelector('.five-day');
 
 
+function getWeather() {
+  var city = cityInputEl.value;
+  var fiveday = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIkey;
 
-
-function getWeather(){
-
- var city=cityInputEl.value.trim();
-   if(city) {
-      cityInfoEl(city);
-      previousCityEl.textContent = '';
-   }
-   
-
-    fetch(fiveday)
-     .then(function(response){
-        if(response.ok) {
-            console.log(response.status);
-            return response.json();
-        } 
-     }) .then(function(data){
-        console.log(data);
-     })
+  fetch(fiveday)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response.status);
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 };
 
-getWeather();
+function displayResults(data) {
+   var fiveResults = document.createElement('li');
+   fiveResults.classList.add('weather');
 
+   var cityName = data.city.name;
+   var weatherCondition = data.list[0].weather[0].main;
+   var weatherDescription = data.list[0].weather[0].description;
+   var temperature = data.list[0].main.temp;
+
+   weatherEl.innerHTML = '<h2>' + cityName + '</h2>' + '<p>weather: ' + weatherCondition + ' _ ' + weatherDescription + '</p>' +
+   '<p>temperature:' + temperature + '</p>';
+
+   previousCityEl.appendChild(fiveResults)
+    .then(function(data){
+      displayResults(data);
+    });
+};
+
+submitBtn.addEventListener('click', getWeather);
